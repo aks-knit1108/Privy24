936ee230c2589ca82ac22314d0022ec0371f52c9
Amit <amit@Amits-MacBook-Pro.local>
2015-08-27 20:42:46 +0530

    Initial Commit

---
+++ Privy24/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Privy24
+//
+//  Created by Amit on 8/27/15.
+//  Copyright (c) 2015 BrightZone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Privy24Tests/Privy24Tests.m
@@ -0,0 +1,40 @@
+//
+//  Privy24Tests.m
+//  Privy24Tests
+//
+//  Created by Amit on 8/27/15.
+//  Copyright (c) 2015 BrightZone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Privy24Tests : XCTestCase
+
+@end
+
+@implementation Privy24Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
