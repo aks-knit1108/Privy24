f1331a2005bc07c9ef6bdf0659fcd86c3b0caa46
Apple <apple@Apples-MacBook-Pro.local>
2016-04-10 17:18:09 +0530

    Initial Commit

---
+++ NotificationPopUp.h
@@ -0,0 +1,13 @@
+//
+//  NotificationPopUp.h
+//  
+//
+//  Created by Apple on 07/04/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationPopUp : UIView
+
+@end
---
+++ NotificationPopUp.m
@@ -0,0 +1,21 @@
+//
+//  NotificationPopUp.m
+//  
+//
+//  Created by Apple on 07/04/16.
+//
+//
+
+#import "NotificationPopUp.h"
+
+@implementation NotificationPopUp
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
